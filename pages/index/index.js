abb62033b043bda61bf2c61d2c408222f79771a5
Alice <719376965@qq.com>
2017-05-12 13:26:08 +0800

    5/12/13:26

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,11 +7,11 @@ Page({
     // 页面初始化 options为页面跳转所带来的参数
   },
   showmap:function(){
-      test.showmap()
+      //test.showmap()
   },
   onReady:function(){
     // 页面渲染完成
-    test.showmap()
+    //test.showmap()
   },
   onShow:function(){
     // 页面显示
